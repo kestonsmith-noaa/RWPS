4aedb5ab52b7b34f2bc5b210e25a1fbeda4b0280
kestonsmith-noaa <keston.smith@noaa.gov>
2026-08-19 19:11:28 +0000

    fixed filename convention for get_rtofs.sh

--- ush/preprocess/get_currents.sh
+++ ush/preprocess/get_currents.sh
@@ -3,6 +3,6 @@
 # This script retrieves global RTOFS and STOFS as netcdf files
 
 #source ./rwpsenv
-rtofs/GetRTOFS.sh $PDY &
+rtofs/get_rtofs.sh $PDY &
 stofs/get_stofs.sh $PDY $cyc current&
 wait;
